8fc050ba31eccd87579d32fbf775cd41f60e94e2
prencipeilariarita <s1068983@studenti.univpm.it>
2021-02-26 18:26:38 +0100

    Aggiungo file mvn

---
+++ src/main/java/it/univpm/SaveYourLook/SaveYourLookApplication.java
@@ -0,0 +1,13 @@
+package it.univpm.SaveYourLook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaveYourLookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaveYourLookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/univpm/SaveYourLook/SaveYourLookApplicationTests.java
@@ -0,0 +1,13 @@
+package it.univpm.SaveYourLook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaveYourLookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
